8e3fbf297c2d2d6bbe778fcab92aa1b8a312c767
Simone Zecchinato <simone.zecchi4to@gmail.com>
2025-08-26 16:49:58 +0200

    Creazione interfaccia supportoMultimediale

--- model/include/supportoMultimediale.hpp
+++ model/include/supportoMultimediale.hpp
@@ -2,14 +2,16 @@
 #define SUPP_MULT_HPP
 
 #include "digitale.hpp"
+#include "../../visitor/visitorCMultimediale.hpp"
 #include <vector>
 
 using std::vector;
 
 class supportoMultimediale{
 public:
-    
-
+    virtual vector<contenutoMultimediale*> getTracce() const = 0;
+    virtual void setTracce(const vector<contenutoMultimediale*>) = 0;
+    virtual void accept(visitorCMultimediale&) = 0;
 };
 
 #endif
