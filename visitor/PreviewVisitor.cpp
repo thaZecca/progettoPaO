81dfdfbf02906cba3e97a9ff51abbf6255d48bbc
Simone Zecchinato <simone.zecchi4to@gmail.com>
2025-08-31 12:09:41 +0200

    Metodo prepare che prepara la preview

--- visitor/PreviewVisitor.cpp
+++ visitor/PreviewVisitor.cpp
@@ -7,8 +7,23 @@ PreviewVisitor::PreviewVisitor(QObject* parent): QObject(parent) {}
 /*prepare - prepara la preview*/
 void PreviewVisitor::prepare(){
     if(preview) preview = new QWidget();
-    QVBoxLayout* vLayout = new QHBoxLayout(preview);
-    vLayout -> setAlignment(Qt::AlignLeft | Qt::AlignTop);
+    QHBoxLayout* mainLayout = new QHBoxLayout(preview);
+    mainLayout -> setAlignment(Qt::AlignLeft | Qt::AlignTop);
+
+    mainLayout -> addWidget(pic);
+
+    QVBoxLayout* infoLayout = new QVBoxLayout();
+    infoLayout -> addWidget(titolo);
+    infoLayout -> addWidget(artisti);
+    infoLayout -> addWidget(anno);
+
+    mainLayout -> addLayout(infoLayout);
+
+    QVBoxLayout* rightLayout = new QVBoxLayout();
+    rightLayout -> addWidget(logo);
+    rightLayout -> addWidget(qualita);
+
+    mainLayout -> addLayout(rightLayout);
 }
 
 /*visit - design pattern per visitare l'audioD
@@ -80,6 +95,8 @@ void PreviewVisitor::visit(fileAudio* f){
     qualita -> setText(f -> getEstensione());
 }
 
+/*visit - design pattern per visitare il fileVideo
+@param f oggetto fileVideo da visitare*/
 void PreviewVisitor::visit(fileVideo* f){
     pic = new QLabel();
     QPixmap image(biblioteca::projectPath().filePath("/src/placeholder.png"));
@@ -101,12 +118,47 @@ void PreviewVisitor::visit(fileVideo* f){
     qualita -> setText(f -> getEstensione());
 }
 
-void PreviewVisitor::visit(cd* c){}
+/*visit - design pattern per visitare il CD
+@param c oggetto CD da visitare*/
+void PreviewVisitor::visit(cd* c){
+    pic = new QLabel();
+    QPixmap image(biblioteca::projectPath().filePath("/src/placeholder.png"));
+    pic -> setPixmap(image);
+    titolo = new QLabel()
+    titolo -> setText(c -> getTitolo());
+    artisti = new QLabel();
+    artisti -> setText("");
+    anno = new QLabel();
+    anno -> setText("");
+    logo = new QLabel();
+    QPixmap logo(biblioteca::projectPath().filePath("/src/placeholder.png"));
+    logo -> setPixmap(logo);
+    qualita = new QLabel();
+    qualita -> setText(c -> getDiametro());
+}
 
-void PreviewVisitor::visit(dvd* d){}
+/*visit - design pattern per visitare il DVD
+@param d oggetto DVD da visitare*/
+void PreviewVisitor::visit(dvd* d){
+    pic = new QLabel();
+    QPixmap image(biblioteca::projectPath().filePath("/src/placeholder.png"));
+    pic -> setPixmap(image);
+    titolo = new QLabel()
+    titolo -> setText(d -> getTitolo());
+    artisti = new QLabel();
+    artisti -> setText("");
+    anno = new QLabel();
+    anno -> setText("");
+    logo = new QLabel();
+    QPixmap logo(biblioteca::projectPath().filePath("/src/placeholder.png"));
+    logo -> setPixmap(logo);
+    qualita = new QLabel();
+    qualita -> setText(d -> getDiametro());
+}
 
 /*getPreview
 @return la preview del contenutoMultimediale o del supportoMultimediale*/
 QWidget* PreviewVisitor::getPreview(){
+    prepare();
     return preview;
 }
