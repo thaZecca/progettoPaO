2ec55a59b0e0b40e75d437e8c0574a7948e7ff6a
Simone Zecchinato <simone.zecchi4to@gmail.com>
2025-09-05 08:57:23 +0200

    Aggiunta della memoria dell'ultima query, getter della stessa, segnali di click e slot per far rimbalzare il click

--- gui/ItemView.cpp
+++ gui/ItemView.cpp
@@ -2,7 +2,7 @@
 
 /*Costruttore parametrico
 @param parent*/
-ItemView::ItemView(QWidget* parent): QScrollArea(parent){
+ItemView::ItemView(QWidget* parent): QScrollArea(parent), lastQuery(nullptr){
     setWidgetResizable(true); //ridimensionamento automatico
     prepare(); //preparo il widget delle previews nella scrollbar area
 }
@@ -21,26 +21,51 @@ void ItemView::prepare(query* q){
 
     if(q) q -> filter(con,sup);
 
+    ClickablePreview* p;
+
     //aggiungo al layout ogni preview di contenutoMultimediale
+    int count = 0;
     for(auto c = con.begin(); c != con.end(); c++){
         (*c) -> accept(v);
-        previewLayout -> addWidget(v.getPreview());
+        p = v.getPreview();
+        p -> setObjectName(QString::number(count));
+        previewLayout -> addWidget(p);
+        count++;
+        connect(p, &ClickablePreview::clicked, this, &ItemView::click_preview_event); //connetto tutte le preview ad un unico slot
     }
     for(auto s = sup.begin(); s != sup.end(); s++){
         (*s) -> accept(v);
-        previewLayout -> addWidget(v.getPreview());
+        p = v.getPreview();
+        p -> setObjectName(QString::number(count));
+        previewLayout -> addWidget(p);
+        count++;
+        connect(p, &ClickablePreview::clicked, this, &ItemView::click_preview_event); //connetto tutte le preview ad un unico slot
     }
     previewLayout -> addStretch(); //stretcho il layout totale
     setWidget(previews);
+}
+
+/*getLastQuery
+@return l'ultima query effettuata*/
+query* ItemView::getLastQuery() const{
+    return lastQuery;
 }
 
 /*submitQuery - slot che ridireziona la query
 @param q query*/
 void ItemView::submitQuery(query q){
     prepare(&q);
+    lastQuery = &q;
 }
 
 /*reload_preview - slot per ricaricare l'interfaccia a seguito di ricaricamenti dal file di storage*/
 void ItemView::reload_preview(){
     prepare();
+}
+
+/*click_preview_event - slot per ricevere l'evento di click sulle preview
+@param ip indice della preview cliccata*/
+void ItemView::click_preview_event(int ip){
+    std::cout << "click(recived)"+std::to_string(ip) << std::endl;
+    emit(clicked(ip));
 }
--- gui/include/ItemView.hpp
+++ gui/include/ItemView.hpp
@@ -6,18 +6,25 @@
 #include <QVBoxLayout>
 #include <QScrollArea>
 #include <QString>
+#include "ClickablePreview.hpp"
 #include "../../model/include/query.hpp"
 #include "../../fileManager/include/biblioteca.hpp"
 #include "../../visitor/include/PreviewVisitor.hpp"
 
 class ItemView: public QScrollArea{
     Q_OBJECT 
+private:
+    query* lastQuery;
 public:
     explicit ItemView(QWidget* parent =nullptr);
     void prepare(query* =nullptr);
+    query* getLastQuery() const;
+signals:
+    void clicked(int ip);
 public slots:
     void submitQuery(query q);
     void reload_preview();
+    void click_preview_event(int ip);
 };
 
 #endif
