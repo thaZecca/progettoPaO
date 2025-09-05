6e164650c8b277be9112c1360555303c793a14b9
Simone Zecchinato <simone.zecchi4to@gmail.com>
2025-09-05 08:55:37 +0200

    Sistemazione struttura main window, metodi e attributi

--- gui/MainWindow.cpp
+++ gui/MainWindow.cpp
@@ -4,22 +4,7 @@
 MainWindow::MainWindow(QWidget* parent): QWidget(parent){
     resize(1050, 700); // Imposta la dimensione iniziale della finestra
     setMinimumSize(600, 400); // Imposta la dimensione minima
-    ItemView* previews = new ItemView();
-    StatView* statistics = new StatView();
-    SearchView* search = new SearchView();
-    MainButtonView* buttons = new MainButtonView();
-
-    QHBoxLayout* mainLayout = new QHBoxLayout(this);
-    mainLayout -> addWidget(previews);
-
-    QWidget* right = new QWidget();
-    right -> setMaximumWidth(330);
-    QVBoxLayout* rightLayout = new QVBoxLayout(right);
-    rightLayout -> addWidget(statistics);
-    rightLayout -> addWidget(search);
-    rightLayout -> addWidget(buttons);
-
-    mainLayout -> addWidget(right);
+    prepareMainWindow();
 
     //Connessioni tra segnali e slots delle classi
     connect(search, &SearchView::search_event, previews, &ItemView::submitQuery);
@@ -27,6 +12,27 @@ MainWindow::MainWindow(QWidget* parent): QWidget(parent){
     connect(buttons, &MainButtonView::reload_event, this, &MainWindow::reload);
     connect(this, &MainWindow::reload_preview_event, previews, &ItemView::reload_preview);
 } 
+
+void MainWindow::prepareMainWindow(){
+    mainLayout = new QHBoxLayout(this);
+    previews = new ItemView();
+    statistics = new StatView();
+    search = new SearchView();
+    buttons = new MainButtonView();
+
+    mainLayout -> addWidget(previews);
+    prepareMainRightWindow();
+    mainLayout -> addWidget(right);
+}
+
+void MainWindow::prepareMainRightWindow(){
+    right = new QWidget();
+    right -> setMaximumWidth(330);
+    QVBoxLayout* rightLayout = new QVBoxLayout(right);
+    rightLayout -> addWidget(statistics);
+    rightLayout -> addWidget(search);
+    rightLayout -> addWidget(buttons);
+}
 
 /*save - slot per il savataggio*/
 void MainWindow::save(){
@@ -56,4 +62,8 @@ void MainWindow::reload(){
         default:
             break;
     }
+}
+
+void MainWindow::show_full_preview(int ip){
+
 }
--- gui/include/MainWindow.hpp
+++ gui/include/MainWindow.hpp
@@ -13,13 +13,24 @@
 
 class MainWindow: public QWidget{
     Q_OBJECT
+private:
+    QHBoxLayout* mainLayout;
+    QWidget* right;
+    ItemView* previews;
+    StatView* statistics;
+    SearchView* search;
+    MainButtonView* buttons;
+    void prepareMainRightWindow();
+    void prepareFullPreview();
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+    void prepareMainWindow();
 signals:
     void reload_preview_event();
 public slots:
     void save();
     void reload();
+    void show_full_preview(int ip);
 };
 
 #endif
