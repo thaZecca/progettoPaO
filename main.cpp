4cf010374a712d032f3369b7f7175810907ea3c8
Simone Zecchinato <simone.zecchi4to@gmail.com>
2025-09-16 07:46:31 +0200

    Prova sits. ambiente grafico

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "fileManager/include/biblioteca.hpp"
 
 int main(int argc, char* argv[]){
+    qputenv("QT_QPA_PLATFORM", "xcb"); //per evitare warning a causa dell'ambiente grafico 
     QApplication app(argc,argv);
 
     MainWindow mw;
