6e2b8df5e776addcd64375f14b562ad806a73384
Simone Zecchinato <simone.zecchi4to@gmail.com>
2025-09-16 07:59:24 +0200

    a

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]){
     #if defined(Q_OS_MAC) //direttiva per sistema operativo Apple macOS
         qputenv("QT_QPA_PLATFORM", "cocoa");
     #else
-        qputenv("QT_QPA_PLATFORM", "wayland"); //per evitare warning a causa dell'ambiente grafico 
+        qputenv("QT_QPA_PLATFORM", "xcb"); //per evitare warning a causa dell'ambiente grafico 
     #endif
     QApplication app(argc,argv);
 
